a179876f7ea03a1cd846571d141e75bf7cfd0b6d
Alexander Michael Edgecombe <alexander.edgecombe01@gmail.com>
2024-03-14 12:27:17 +0000

    Practical

--- Coursework/index.js
+++ Coursework/index.js
@@ -1,26 +1,32 @@
-// Section for objects
+/* Section for objects
 
-// Workout object
+Workout object
 const Workout = {
-    // A workout will have one or more activities
-    // You will be able to add, remove and modify activities
-    // A workout will be able to start, stop and pause
+     A workout will have one or more activities
+     You will be able to add, remove and modify activities
+     A workout will be able to start, stop and pause
 }
 
-// Activity object
+Activity object
 const Activity = {
-    // An activity will have a name, duration and a description
-    // An activity will be able to start, stop and pause
-    // An activity will be able to be added to a workout
-    // An activity will be able to be removed from a workout
-    // An activity will be able to be modified
+     An activity will have a name, duration and a description
+     An activity will be able to start, stop and pause
+     An activity will be able to be added to a workout
+     An activity will be able to be removed from a workout
+     An activity will be able to be modified
 }
 
+*/
 
-// Function section
+/* Function section
 
-// There needs to be a collection of Activites and Workouts stored on the server
+ There needs to be a collection of Activites and Workouts stored on the server
+ The user should be able to add Activities to the server collection
+ The user should be able to add Workouts to the server collection
+ Timer function
+ Function for templates
+ Remove anything that isn't selected template
+ Include button to naviagte to the next template
+ Repeat for relevant templates
 
-// The user should be able to add Activities to the server collection 
-
-// The user should be able to add Workouts to the server collection
+*/
